bacdb833d627b3c5363015d9d9d401a2ee0222b8
Arthur Neuhauss <arthur.carlos.dj@gmail.com>
2025-05-26 21:47:11 -0300

    Atualizei a função do Sort, mudei o nome de algumas variaveis

--- Lista04/teste.cpp
+++ Lista04/teste.cpp
@@ -9,12 +9,22 @@ int tempoParaSegundos(int h, int m, int s)
     return h * 3600 + m * 60 + s;
 }
 
+bool comparador(const pair<int, int> &a, const pair<int, int> &b)
+{
+    if (a.first == b.first)
+    {
+        return a.second < b.second;
+    }
+    return a.first < b.first;
+}
+
 int main()
 {
     ios::sync_with_stdio(false);
-    int entrada;
-    int ent_hora, ent_minuto, ent_segundos;
-    int sai_hora, sai_minuto, sai_segundos;
+    int entrada, cadeiras = 0, contador = 0;
+    int chegada, ent_hora, ent_minuto, ent_segundos; // deixei separado pra ficar bonitinho mesmo
+    int saida, sai_hora, sai_minuto, sai_segundos;
+
     cin >> entrada;
 
     vector<pair<int, int>> clientes;
@@ -24,26 +34,22 @@ int main()
         scanf("%d:%d:%d", &ent_hora, &ent_minuto, &ent_segundos);
         scanf("%d:%d:%d", &sai_hora, &sai_minuto, &sai_segundos);
 
-        int chegada = tempoParaSegundos(ent_hora, ent_minuto, ent_segundos);
-        int saida = tempoParaSegundos(sai_hora, sai_minuto, sai_segundos);
+        chegada = tempoParaSegundos(ent_hora, ent_minuto, ent_segundos);
+        saida = tempoParaSegundos(sai_hora, sai_minuto, sai_segundos);
 
         clientes.emplace_back(chegada, 1);
         clientes.emplace_back(saida, -1);
     }
 
-    sort(clientes.begin(), clientes.end(), [](const pair<int, int> &a, const pair<int, int> &b)
-         {
-        if (a.first == b.first) return a.second < b.second;
-        return a.first < b.first; });
+    sort(clientes.begin(), clientes.end(), comparador);
 
-    int cadeiras = 0, max_cadeiras = 0;
     for (const auto &cliente : clientes)
     {
         cadeiras += cliente.second;
-        max_cadeiras = max(max_cadeiras, cadeiras);
+        contador = max(contador, cadeiras);
     }
 
-    cout << max_cadeiras << endl;
+    cout << contador << endl;
 
     return 0;
 }
